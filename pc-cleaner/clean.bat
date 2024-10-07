057e7f651e8d809f34ece4a301b16256ab9d115f
FurryBoy <bogdanfurry2000@gmail.com>
2024-10-07 03:37:52 +0300

    update 10/7/2024

--- pc-cleaner/clean.bat
+++ pc-cleaner/clean.bat
@@ -3,7 +3,7 @@
 cls
 title PC Cleaner - Loading... - https://github.com/FurryBoyYT/pc-cleaner
 
-net session >nul 2>&1
+net session > nul 2>&1
 if %errorLevel% == 0 (
     goto :admin
 ) else (
@@ -13,7 +13,7 @@ if %errorLevel% == 0 (
 )
 
 :admin
-echo A restore point will be created incase something bad happened, you can restore your pc back after!
+echo A restore point is being created incase something bad happened with the PC Cleaner, you can restore your PC back after!
 wmic /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "PC Cleaner restore point", 100, 7
 cls
 
@@ -21,7 +21,7 @@ title PC Cleaner - Starting... - https://github.com/FurryBoyYT/pc-cleaner
 for /l %%i in (5,-1,1) do (
     echo [1;32mCleaning process will start in [0;33m%%i seconds![1;32m
     echo [1;31mPlease do not close while it's cleaning![0m
-    timeout /nobreak /t 1 >nul
+    timeout /nobreak /t 1 > nul
     cls
 )
 title PC Cleaner - Cleaning... - https://github.com/FurryBoyYT/pc-cleaner
@@ -60,75 +60,15 @@ echo [1;32mCleaning windows registry cache...[1;31m
 reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs" /f
 reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU" /f
 
-:----web-browsers----
-echo.
-echo [1;32mCleaning NVIDIA GeForce Experience cache...[1;31m
-del /q /s "%localappdata%\NVIDIA Corporation\NVIDIA GeForce Experience\CefCache\Cache\*"
-del /q /s "%localappdata%\NVIDIA Corporation\NVIDIA GeForce Experience\CefCache\GPUCache\*"
-
-echo.
-echo [1;32mCleaning Visual Studio Code cache...[1;31m
-del /q /s "%appdata%\Code\Cache\Cache_Data\*"
-
-echo.
-echo [1;32mCleaning Python pip cache...[1;31m
-del /q /s "%localappdata%\pip\cache\*"
-for /d %%i in (%localappdata%\pip\cache\*) do ( rd /s /q "%%i" )
-
-echo.
-echo [1;32mCleaning Nodejs npm cache...[1;31m
-del /q /s "%localappdata%\npm-cache\*"
-for /d %%i in (%localappdata%\npm-cache\*) do ( rd /s /q "%%i" )
-
-echo.
-echo [1;32mCleaning applications cache...
-echo Google[1;31m
-del /q /s "%localappdata%\Google\Chrome\User Data\Default\Cache\*"
-
-echo.
-echo [1;32mFirefox[1;31m
-del /q /s "%localappdata%\Mozilla\Firefox\Profiles\*\cache2\entries\*"
-
-echo.
-echo [1;32mMicrosoft[1;31m
-del /q /s "%localappdata%\Microsoft\Edge\User Data\Default\Cache\*"
-
-echo.
-echo [1;32mINetCache[1;31m
-rd /s /q "%localappdata%\Microsoft\Windows\INetCache"
-
-echo.
-echo [1;32mOpera[1;31m
-del /q /s "%appdata%\Opera Software\Opera Stable\Cache\*"
-
-echo.
-echo [1;32mOpera GX[1;31m
-del /q /s "%appdata%\Opera Software\Opera GX Stable\Cache\*"
-
-echo.
-echo [1;32mBrave[1;31m
-del /q /s "%localappdata%\BraveSoftware\Brave-Browser\User Data\Default\Cache\*"
-
-echo.
-echo [1;32mVivaldi[1;31m
-del /q /s "%localappdata%\Vivaldi\User Data\Default\Cache\*"
-
-echo.
-echo [1;32mTor[1;31m
-del /q /s "%localappdata%\Tor Browser\Browser\TorBrowser\Data\Browser\profile.default\cache2\entries\*"
-
-echo.
-echo [1;32mSteam[1;31m
-del /q /s "%localappdata%\Steam\htmlcache\*"
-
-echo.
-echo [1;32mGithub Desktop[1;31m
-del /q /s "%appdata%\GitHub Desktop\logs\*"
-
-echo.
-echo [1;32mOneDrive[1;31m
-del /q /s "%localappdata%\Microsoft\OneDrive\settings\Business1\*"
-del /q /s "%localappdata%\Microsoft\OneDrive\logs\Common\*"
+reg query "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" > nul
+if %errorlevel% == 0 (
+    echo.
+    set "IS_PENDING_UPDATES=0"
+    echo [1;32mCleaning software distribution download cache...[1;31m
+    del /s /f /q "%systemroot%\SoftwareDistribution\Download"
+) else (
+    set "IS_PENDING_UPDATES=1"
+)
 
 :-----roblox-----
 echo.
@@ -168,25 +108,29 @@ rd /s /q "%systemdrive%\$Recycle.bin"
 echo.
 echo [1;32mConfigurating IP Config... (hidden for security reasons)[1;31m
 echo WARNING! Your internet may interrupt during this operation.
-ipconfig /flushdns >nul
+ipconfig /flushdns > nul
 echo Flushed DNS!
-ipconfig /registerdns >nul
+ipconfig /registerdns > nul
 echo Registered DNS!
-ipconfig /release >nul
+ipconfig /release > nul
 echo Released!
-ipconfig /renew >nul
+ipconfig /renew > nul
 echo Renewed!
-netsh winsock reset >nul
+netsh winsock reset > nul
 echo Reset winsock!
-powershell -Command "Disable-MMAgent -MemoryCompression" >nul
+powershell -Command "Disable-MMAgent -MemoryCompression" > nul
 echo Memory compression disabled!
-echo [0;31mIP Config configuration complete! Restart is required to take changes.
+echo [0;31mIP Config configuration complete! A PC restart is required to take changes.
 
 title PC Cleaner - Finished! - https://github.com/FurryBoyYT/pc-cleaner
 echo.
 echo [1;32mPC Cleaning finished!
-echo [0;31mWeb browsers/Discord might be buggy because of cache cleaning![1;32m
 echo Exiting in 10 seconds.
-timeout /nobreak /t 10 >nul
+if %IS_PENDING_UPDATES% == 1 (
+    echo [0;31mThere was a problem while attempting to clean some of the files:
+    echo Unable to clean software distribution download cache:
+    echo There are pending windows updates, please update and try running the cleaner again!
+)
+timeout /nobreak /t 10 > nul
 exit /b
-:-----end----- 
+:-----end-----
