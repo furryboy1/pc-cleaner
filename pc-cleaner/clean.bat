4d8564eccdaf90fe721b20e8428e5bafe8a6982a
FurryBoy <104305700+FurryBoyYT@users.noreply.github.com>
2025-04-18 19:11:55 +0300

    Update clean.bat

--- pc-cleaner/clean.bat
+++ pc-cleaner/clean.bat
@@ -5,7 +5,9 @@ cls
 :: --- Initialization ----
 title PC Cleaner - Loading... - https://github.com/FurryBoyYT/pc-cleaner
 
-echo [1;33mWARNING^! The code was rewritten with some new paths as of April 5, 2025 and hasn't been tested.[0m
+setlocal disabledelayedexpansion
+echo [1;33mWARNING!!! The code was rewritten with some new paths as of April 5, 2025 and hasn't been tested.[0m
+endlocal
 echo [1;33mUse at your own risk and report any issues on the GitHub issues page if there are any within the script.[0m
 echo [1;32m^-^-^-^> [4;34m[1;36mhttps://github.com/FurryBoyYT/pc-cleaner/issues [1;32m^<^-^-^-[0m
 echo [0mPress any key to continue or close this window to cancel.[0m
