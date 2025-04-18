f2f0094b56be9e1fc14fee26dd76d65e375ce270
FurryBoy <104305700+FurryBoyYT@users.noreply.github.com>
2025-04-18 19:17:38 +0300

    Update clean.bat

--- pc-cleaner/clean.bat
+++ pc-cleaner/clean.bat
@@ -4,10 +4,10 @@ cls
 :: --- Initialization ----
 title PC Cleaner - Loading... - https://github.com/FurryBoyYT/pc-cleaner
 
-echo [1;33mWARNING!!! The code was rewritten with some new paths as of April 5, 2025 and hasn't been tested.[0m
-echo [1;33mUse at your own risk and report any issues on the GitHub issues page if there are any within the script.[0m
-echo [1;32m^-^-^-^> [4;34m[1;36mhttps://github.com/FurryBoyYT/pc-cleaner/issues [1;32m^<^-^-^-[0m
-echo [0mPress any key to continue or close this window to cancel.[0m
+echo [1;33mWARNING!!! The code was rewritten with some new paths as of April 5, 2025 and hasn't been tested.
+echo [1;33mUse at your own risk and report any issues on the GitHub issues page if there are any within the script.
+echo [1;32m^-^-^-^> [4;34m[1;36mhttps://github.com/FurryBoyYT/pc-cleaner/issues [1;32m^<^-^-^-
+echo [0mPress any key to continue or close this window to cancel.
 pause >nul
 echo [1;32mNow continuing with the script, [1;33myou have been warned.[0m
 
